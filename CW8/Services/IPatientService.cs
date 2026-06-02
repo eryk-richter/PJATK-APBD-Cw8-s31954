bce41a5955e44e2cfd5b81473605f57b3f9602da
Eryk <s31954@pjwstk.edu.pl>
2026-06-03 00:35:53 +0200

    implement GetAllAsync service method

--- CW8/Services/IPatientService.cs
+++ CW8/Services/IPatientService.cs
@@ -1,5 +1,10 @@
-﻿namespace CW8.Services;
+﻿using CW8.DTOs;
+
+namespace CW8.Services;
 
 public interface IPatientService {
+    
+    public Task<IEnumerable<PatientDetailsDto>> GetAllAsync(CancellationToken cancellationToken);
+    public Task<PatientDetailsDto> GetPatientByNameAsync(string name, CancellationToken cancellationToken);
     
 }
--- CW8/Services/PatientService.cs
+++ CW8/Services/PatientService.cs
@@ -1,5 +1,54 @@
-﻿namespace CW8.Services;
+﻿using CW8.DTOs;
+using CW8.Infrastructure;
+using Microsoft.EntityFrameworkCore;
 
-public class PatientService: IPatientService {
-    
+namespace CW8.Services;
+
+public class PatientService (Apbd8Context ctx): IPatientService {
+    public async Task<IEnumerable<PatientDetailsDto>> GetAllAsync(CancellationToken cancellationToken) {
+        return await ctx.Patients.Select(p => new PatientDetailsDto(
+            p.Pesel,
+            p.FirstName,
+            p.LastName,
+            p.Age,
+            p.Sex ? "Male" : "Female",
+            p.Admissions.Select(pa => new AdmissionDto(
+                pa.Id,
+                pa.AdmissionDate,
+                pa.DischargeDate,
+                new WardDto(
+                    pa.Ward.Id,
+                    pa.Ward.Name,
+                    pa.Ward.Description)
+            )),
+            p.BedAssignments.Select(pb => new BedAssignmentDto(
+                pb.Id,
+                pb.From,
+                pb.To,
+                new BedDto(
+                    pb.Bed.Id,
+                    new BedTypeDto(
+                        pb.Bed.BedType.Id,
+                        pb.Bed.BedType.Name,
+                        pb.Bed.BedType.Description
+                    ),
+                    new RoomDto(
+                        pb.Bed.Room.Id,
+                        pb.Bed.Room.HasTv,
+                        new WardDto(
+                            pb.Bed.Room.Ward.Id,
+                            pb.Bed.Room.Ward.Name,
+                            pb.Bed.Room.Ward.Description
+                        )
+
+                    )
+
+                )
+            ))
+        )).ToListAsync();
+    }
+
+    public Task<PatientDetailsDto> GetPatientByNameAsync(string name, CancellationToken cancellationToken) {
+        throw new NotImplementedException();
+    }
 }
