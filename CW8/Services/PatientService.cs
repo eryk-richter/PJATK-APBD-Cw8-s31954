bdb2f9b9106480503482105aa0c0944f6c78f569
Eryk <s31954@pjwstk.edu.pl>
2026-06-03 02:05:54 +0200

    fix GET results order

--- CW8/Services/PatientService.cs
+++ CW8/Services/PatientService.cs
@@ -20,45 +20,47 @@ public class PatientService (Apbd8Context ctx): IPatientService {
         }
         
         return await query
+            .OrderByDescending(p => p.Pesel)
             .Select(p => new PatientDetailsDto(
-            p.Pesel,
-            p.FirstName,
-            p.LastName,
-            p.Age,
-            p.Sex ? "Male" : "Female",
-            p.Admissions.Select(pa => new AdmissionDto(
-                pa.Id,
-                pa.AdmissionDate,
-                pa.DischargeDate,
-                new WardDto(
-                    pa.Ward.Id,
-                    pa.Ward.Name,
-                    pa.Ward.Description)
-            )),
-            p.BedAssignments.Select(pb => new BedAssignmentDto(
-                pb.Id,
-                pb.From,
-                pb.To,
-                new BedDto(
-                    pb.Bed.Id,
-                    new BedTypeDto(
-                        pb.Bed.BedType.Id,
-                        pb.Bed.BedType.Name,
-                        pb.Bed.BedType.Description
-                    ),
-                    new RoomDto(
-                        pb.Bed.Room.Id,
-                        pb.Bed.Room.HasTv,
-                        new WardDto(
-                            pb.Bed.Room.Ward.Id,
-                            pb.Bed.Room.Ward.Name,
-                            pb.Bed.Room.Ward.Description
+                p.Pesel,
+                p.FirstName,
+                p.LastName,
+                p.Age,
+                p.Sex ? "Male" : "Female",
+                p.Admissions.Select(pa => new AdmissionDto(
+                    pa.Id,
+                    pa.AdmissionDate,
+                    pa.DischargeDate,
+                    new WardDto(
+                        pa.Ward.Id,
+                        pa.Ward.Name,
+                        pa.Ward.Description)
+                )),
+                p.BedAssignments.Select(pb => new BedAssignmentDto(
+                    pb.Id,
+                    pb.From,
+                    pb.To,
+                    new BedDto(
+                        pb.Bed.Id,
+                        new BedTypeDto(
+                            pb.Bed.BedType.Id,
+                            pb.Bed.BedType.Name,
+                            pb.Bed.BedType.Description
+                        ),
+                        new RoomDto(
+                            pb.Bed.Room.Id,
+                            pb.Bed.Room.HasTv,
+                            new WardDto(
+                                pb.Bed.Room.Ward.Id,
+                                pb.Bed.Room.Ward.Name,
+                                pb.Bed.Room.Ward.Description
+                            )
+
                         )
 
                     )
-
                 )
-            ))
+            )
         )).ToListAsync(cancellationToken);
         
     }
